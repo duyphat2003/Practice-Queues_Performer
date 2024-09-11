f29a9a90f7760b83e593bb59c718e9ff0f2ee53d
duyphat2003 <rofduyphat2003@gmail.com>
2024-09-11 14:14:57 +0700

    init

---
+++ .local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace PracticeQueues_Performer
+{
+    public class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
